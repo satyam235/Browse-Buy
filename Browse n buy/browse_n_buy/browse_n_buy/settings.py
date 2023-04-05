bb5a620eec80a9adb021b77851fff881c92f5dfe
sam235 <satyamshukla95183@gmail.com>
2023-04-05 23:45:20 +0000

    fix

--- Browse n buy/browse_n_buy/browse_n_buy/settings.py
+++ Browse n buy/browse_n_buy/browse_n_buy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2zhgetl^iq4^(mj)-bkj(fe0=#f6vta32kn94^%*!h1txb_&(d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['20.168.231.227']
 
 
 # Application definition
